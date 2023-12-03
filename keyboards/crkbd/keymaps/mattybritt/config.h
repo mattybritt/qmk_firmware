71550c547030e45cea2f5a054cb1cd5650f4aae2
mattb <mattybritt@gmail.com>
2023-12-03 19:41:46 +0200

    Set to 250

--- keyboards/crkbd/keymaps/mattybritt/config.h
+++ keyboards/crkbd/keymaps/mattybritt/config.h
@@ -2,5 +2,4 @@
 
 #define MASTER_LEFT
 
-#define TAPPING_TERM 300 // initial was 200 then was 400 which was too slow so now 300
-
+#define TAPPING_TERM 250 // initial was 200 then was 400 which was too slow so now 300, then 250
